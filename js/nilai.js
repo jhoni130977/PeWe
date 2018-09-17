fd8e93c1e56d27e5d8604c06456e3ed4114c21c6
jhoni130977 <31085099+jhoni130977@users.noreply.github.com>
2018-09-17 09:42:13 +0700

    Add files via upload

---
+++ js/nilai.js
@@ -0,0 +1,286 @@
+
+		function displayResult(a){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("1a").disabled=true;
+ document.getElementById("1b").disabled=true;
+ document.getElementById("1c").disabled=true;
+ document.getElementById("1d").disabled=true;
+ document.getElementById("1e").disabled=true;
+
+ document.getElementById("b1").disabled=false;
+ 
+ if(a == "a")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs1").value="0";}
+ if(a == "b")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs1").value="0";
+ }
+ if(a == "c")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs1").value="1";}
+ if(a == "d")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs1").value="0";}
+ if(a == "e")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs1").value="0";}
+}
+
+		function displayResult3(c){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("3a").disabled=true;
+ document.getElementById("3b").disabled=true;
+ document.getElementById("3c").disabled=true;
+ document.getElementById("3d").disabled=true;
+ document.getElementById("3e").disabled=true;
+ document.getElementById("b3").disabled=false;
+ 
+ if(c == "a3")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs3").value="0";}
+ if(c == "b3")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs3").value="0";
+ }
+ if(c == "c3")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs3").value="0";}
+ if(c == "d3")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs3").value="0";}
+ if(c == "e3")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs3").value="1";}
+}
+
+		function displayResult4(d){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("4a").disabled=true;
+ document.getElementById("4b").disabled=true;
+ document.getElementById("4c").disabled=true;
+ document.getElementById("4d").disabled=true;
+ document.getElementById("4e").disabled=true;
+ document.getElementById("b4").disabled=false;
+ 
+ if(d == "a4")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs4").value="0";}
+ if(d == "b4")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs4").value="0";
+ }
+ if(d == "c4")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs4").value="0";}
+ if(d == "d4")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs4").value="0";}
+ if(d == "e4")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs4").value="1";}
+}
+
+		function displayResult5(e){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("5a").disabled=true;
+ document.getElementById("5b").disabled=true;
+ document.getElementById("5c").disabled=true;
+ document.getElementById("5d").disabled=true;
+ document.getElementById("5e").disabled=true;
+ document.getElementById("b5").disabled=false;
+ 
+ if(e == "a5")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs5").value="0";}
+ if(e == "b5")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs5").value="0"; }
+ if(e == "c5")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs5").value="1";}
+ if(e == "d5")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs5").value="0";}
+ if(e == "e5")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs5").value="0";}
+
+}		function displayResult6(f){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("6a").disabled=true;
+ document.getElementById("6b").disabled=true;
+ document.getElementById("6c").disabled=true;
+ document.getElementById("6d").disabled=true;
+ document.getElementById("6e").disabled=true;
+ document.getElementById("b6").disabled=false;
+ 
+ if(f == "a6")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs6").value="0";}
+ if(f == "b6")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs6").value="0"; }
+ if(f == "c6")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs6").value="0";}
+ if(f == "d6")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs6").value="0";}
+ if(f == "e6")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs6").value="1";}
+}
+
+		function displayResult7(g){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("7a").disabled=true;
+ document.getElementById("7b").disabled=true;
+ document.getElementById("7c").disabled=true;
+ document.getElementById("7d").disabled=true;
+ document.getElementById("7e").disabled=true;
+ document.getElementById("b7").disabled=false;
+ 
+ if(g == "a7")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs7").value="0";}
+ if(g == "b7")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs7").value="1"; }
+ if(g == "c7")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs7").value="0";}
+ if(g == "d7")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs7").value="0";}
+ if(g == "e7")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs7").value="0";}
+}
+
+		function displayResult8(h){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("8a").disabled=true;
+ document.getElementById("8b").disabled=true;
+ document.getElementById("8c").disabled=true;
+ document.getElementById("8d").disabled=true;
+ document.getElementById("8e").disabled=true;
+ document.getElementById("b8").disabled=false;
+ 
+ if(h == "a8")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs8").value="0";}
+ if(h == "b8")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs8").value="0"; }
+ if(h == "c8")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs8").value="0";}
+ if(h == "d8")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs8").value="0";}
+ if(h == "e8")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs8").value="1";}
+}
+
+		function displayResult9(i){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("9a").disabled=true;
+ document.getElementById("9b").disabled=true;
+ document.getElementById("9c").disabled=true;
+ document.getElementById("9d").disabled=true;
+ document.getElementById("9e").disabled=true;
+ document.getElementById("b9").disabled=false;
+ 
+ if(i == "a9")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs9").value="1";}
+ if(i == "b9")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs9").value="0"; }
+ if(i == "c9")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs9").value="0";}
+ if(i == "d9")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs9").value="0";}
+ if(i == "e9")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs9").value="0";}
+}
+
+		function displayResult10(j){ 
+ //document.getElementById("ha").innerHTML=a; 
+ document.getElementById("10a").disabled=true;
+ document.getElementById("10b").disabled=true;
+ document.getElementById("10c").disabled=true;
+ document.getElementById("10d").disabled=true;
+ document.getElementById("10e").disabled=true;
+ document.getElementById("b10").disabled=false;
+ 
+ if(j == "a10")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs10").value="1";}
+ if(j == "b10")
+ { //document.getElementById("hb").innerHTML="jawaban anda benar";
+   document.getElementById("hs10").value="0"; }
+ if(j == "c10")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs10").value="0";}
+ if(j == "d10")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs10").value="0";}
+ if(j == "e10")
+ { //document.getElementById("hb").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>Hypert Text Markup Language "; 
+ document.getElementById("hs10").value="0";}
+
+ var hs1a = document.getElementById("hs1").value;
+ var hs2a = document.getElementById("hs2").value;
+ var hs3a = document.getElementById("hs3").value;
+ var hs4a = document.getElementById("hs4").value;
+ var hs5a = document.getElementById("hs5").value;
+ var hs6a = document.getElementById("hs6").value;
+ var hs7a = document.getElementById("hs7").value;
+ var hs8a = document.getElementById("hs8").value;
+ var hs9a = document.getElementById("hs9").value;
+ var hs10a = document.getElementById("hs10").value; 
+ hsl = parseInt(hs1a) + parseInt(hs2a) + parseInt(hs3a) + parseInt(hs4a) + parseInt(hs5a) + parseInt(hs6a) + parseInt(hs7a) + parseInt(hs8a) + parseInt(hs9a) + parseInt(hs10a);
+
+ //hsl = parseInt(hs1a) + parseInt(hs2a) + parseInt(hs3a) + parseInt(hs4a) + parseInt(hs5a) + parseInt(hs6a) + parseInt(hs7a);
+
+ nilai = (hsl/10) * 100;
+ slh =  10-hsl;
+
+ document.getElementById("hss").innerHTML = hsl;
+ document.getElementById("salah").innerHTML = slh;
+ document.getElementById("nl").innerHTML = nilai;
+}
+
+function displayResult2(b){ 
+ document.getElementById("2a").disabled=true;
+ document.getElementById("2b").disabled=true;
+ document.getElementById("2c").disabled=true;
+ document.getElementById("2d").disabled=true;
+ document.getElementById("2e").disabled=true;
+ document.getElementById("b2").disabled=false;
+ 
+ if(b == "a2")
+ { //document.getElementById("hc").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>cascading ";
+ document.getElementById("hs2").value="0";}
+ if(b == "b2")
+ { //document.getElementById("hc").innerHTML="jawaban anda benar";
+   document.getElementById("hs2").value="0";
+ }
+ if(b == "c2")
+ { //document.getElementById("hc").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>cascading ";
+ document.getElementById("hs2").value="1";}
+ if(b == "d2")
+ { //document.getElementById("hc").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>cascading ";
+ document.getElementById("hs2").value="0";}
+ if(b == "e2")
+ { //document.getElementById("hc").innerHTML="jawaban anda Salah<br>yang benar adalah <b>E </b> yaitu <b>cascading Language ";
+ document.getElementById("hs2").value="0";}
+
+
+}
